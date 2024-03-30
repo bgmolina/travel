7f8bfa0a0442e3b29e7b11c9441ef7496280c9e6
Braian Molina <braian_gabriel_1993@hotmail.com>
2024-03-30 16:05:18 -0300

    se agrga carpeta docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
-FROM node:18.14.2-alpine3.17
+# build project
+FROM node:18.14.2-alpine3.17 as build
 WORKDIR /app
 COPY ["package*.json", "."]
-RUN npm install
+RUN npm install --only=production
 COPY [".", "."]
-EXPOSE 8080
-CMD ["npm", "run", "dev"]
+RUN npm run build
+
+# create nginx server
+FROM nginx:1.23.3-alpine
+COPY --from=build ["/app/dist", "/usr/share/nginx/html"]
+COPY ["./nginx.conf", "/etc/nginx/nginx.conf"]
+WORKDIR /usr/share/nginx/html
---
+++ docker/deployment.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+echo "======================"
+echo " Travel deployment 🏖️ "
+echo "======================"
+
+PS3="Select an option and press Enter 👆: "
+OPTIONS=("Deploy on development mode 🛠" "Delete on development mode 🗑️" "Deploy on production mode 🚀" "Delete on production mode 🗑️" "Quit 👋")
+
+: '
+OPTIONS index:
+1 -> Deploy on development mode 🛠
+2 -> Delete on development mode 🗑️
+3 -> Deploy on production mode 🚀
+4 -> Delete on production mode 🗑️
+5 -> Quit 👋
+'
+
+while true; do
+  echo ""
+  select opt in "${OPTIONS[@]}"; do
+    case $REPLY in
+    "1")
+      ARGS="up -d --no-recreate"
+      echo ""
+      echo "Deploying services...🛠"
+      docker compose -f docker-compose.yml $ARGS
+
+      ;;
+    "2")
+      ARGS="down"
+      echo ""
+      echo "Removing services...🗑️"
+      docker compose -f docker-compose.yml $ARGS
+
+      ;;
+    "3")
+      ARGS="up -d --no-recreate "
+      echo ""
+      echo "Deploying services...🚀"
+      docker compose -f docker-compose.prod.yml $ARGS
+
+      ;;
+    "4")
+      ARGS="down"
+      echo ""
+      echo "Removing services...🗑️"
+      docker compose -f docker-compose.prod.yml $ARGS
+
+      ;;
+    "5")
+      echo "bye 👋"
+      exit 0
+      ;;
+    *)
+      echo ""
+      echo "Invalid option 🙃, please try again 👇"
+      ;;
+    esac
+    break
+  done
+done
